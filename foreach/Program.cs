29e1b782298d219a425423e2c0e7218911e6d19f
Yu Zhang <zhangy51@utas.edu.au>
2020-08-20 00:38:41 +1000

    Add project files.

---
+++ foreach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace foreach1
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+            List<string> animals = new List<string> { "elephant", "cat", "mouse", "snake", "giraffe" };
+            //Note that collections like Lists have Count instead of an array's Length property
+            for (int i = 0; i < animals.Count; i++)
+            {
+                Console.WriteLine(animals[i]);
+            }
+            foreach (string s in animals)
+            {
+                Console.WriteLine(s);
+            }
+        }
+}
+}
